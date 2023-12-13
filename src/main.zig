64d738ceb766059b27080fcf0a1cc90f8d8de60e
Isaiah Grace <isaiah@graces.com>
2023-12-13 11:44:40 -0500

    fixed a typo in the cli args

--- src/main.zig
+++ src/main.zig
@@ -6,7 +6,7 @@ const watershed = @import("watershed.zig");
 pub fn main() !void {
     const params = comptime clap.parseParamsComptime(
         \\-h, --help             Display this help and exit.
-        \\-d, --databse <str>    Required. The full path to WBD_National_GPKG.gpkg. This path is given directly to sqlite3_open() and does not support the home directory shortcut '~/'.
+        \\-d, --database <str>   Required. The full path to WBD_National_GPKG.gpkg. This path is given directly to sqlite3_open() and does not support the home directory shortcut '~/'.
         \\-s, --skipHuc14and16   Disables searching in HUC levels 14 and 16. These levels are not defined for most of the US. Defaults to false.
     );
 
@@ -19,15 +19,15 @@ pub fn main() !void {
     };
     defer res.deinit();
 
-    if (res.args.help != 0 or res.args.databse == null) {
+    if (res.args.help != 0 or res.args.database == null) {
         const stderr = std.io.getStdErr().writer();
         try stderr.print("The WatershedOracleCLI program reads WKT point data line by line from stdin and prints watershed data to stdout.\n", .{});
         try stderr.print("The intended use is to pipe WKT point data into this program from another process like cat, or a GPS tracker.\n\n", .{});
         try stderr.print("Usage:\n", .{});
         try clap.help(stderr, clap.Help, &params, .{});
         try stderr.print("\nExamples:\n", .{});
-        try stderr.print("echo \"POINT(-70.386781360 43.5014404586)\" | ./watershedOracleCLI --databse=/home/isaiah/Documents/WBD/WBD_National_GPKG.gpkg\n", .{});
-        try stderr.print("cat gps_data_file | ./watershedOracleCLI --databse=/home/isaiah/Documents/WBD/WBD_National_GPKG.gpkg\n\n", .{});
+        try stderr.print("echo \"POINT(-70.386781360 43.5014404586)\" | zig-out/bin/watershedOracle --database=/home/isaiah/Documents/WBD/WBD_National_GPKG.gpkg\n", .{});
+        try stderr.print("cat gps_data_file | zig-out/bin/watershedOracle --database=/home/isaiah/Documents/WBD/WBD_National_GPKG.gpkg\n\n", .{});
         return;
     }
 
@@ -39,7 +39,7 @@ pub fn main() !void {
     defer if (gpa.deinit() == .leak) std.log.err("GPA detected a leak!", .{});
     const allocator: std.mem.Allocator = gpa.allocator();
 
-    var watershedStack = try watershed.WatershedStack.init(allocator, res.args.databse.?, skipHuc14and16);
+    var watershedStack = try watershed.WatershedStack.init(allocator, res.args.database.?, skipHuc14and16);
     defer watershedStack.deinit();
 
     const stdin = std.io.getStdIn().reader();
