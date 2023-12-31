47326c8cabad72665386f0282c1506f150bf379a
Isaiah Grace <irgkenya4@gmail.com>
2023-12-30 19:31:23 -0500

    Added an SQLite3 call to create indices for the HUC columns if not already present.

--- src/sqliteCtx.zig
+++ src/sqliteCtx.zig
@@ -9,8 +9,25 @@ pub const SqliteCtx = struct {
 
     pub fn init(path: [*:0]const u8) !SqliteCtx {
         var connectionPointer: ?*sqlite.sqlite3 = null;
-        try sqliteErrors.check(sqlite.sqlite3_open_v2(@ptrCast(path), &connectionPointer, sqlite.SQLITE_OPEN_READONLY, null));
+        try sqliteErrors.check(sqlite.sqlite3_open_v2(@ptrCast(path), &connectionPointer, sqlite.SQLITE_OPEN_READWRITE, null));
         if (connectionPointer == null) return error.sqliteOOM;
+
+        // These indicies will help with looking up the HUC codes
+        try sqliteErrors.check(sqlite.sqlite3_exec(
+            connectionPointer,
+            \\CREATE INDEX IF NOT EXISTS huc_index2 ON WBDHU2 (huc2, name);
+            \\CREATE INDEX IF NOT EXISTS huc_index4 ON WBDHU4 (huc4, name);
+            \\CREATE INDEX IF NOT EXISTS huc_index6 ON WBDHU6 (huc6, name);
+            \\CREATE INDEX IF NOT EXISTS huc_index8 ON WBDHU8 (huc8, name);
+            \\CREATE INDEX IF NOT EXISTS huc_index10 ON WBDHU10 (huc10, name);
+            \\CREATE INDEX IF NOT EXISTS huc_index12 ON WBDHU12 (huc12, name);
+            \\CREATE INDEX IF NOT EXISTS huc_index14 ON WBDHU14 (huc14, name);
+            \\CREATE INDEX IF NOT EXISTS huc_index16 ON WBDHU16 (huc16, name);
+        ,
+            null,
+            null,
+            null,
+        ));
 
         return SqliteCtx{
             .conn = connectionPointer,
