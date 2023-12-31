3ef77ad86f0ec12c5c404c78ed0ca8ccf241c59d
Isaiah Grace <irgkenya4@gmail.com>
2023-12-30 21:15:22 -0500

    Made some small tweaks to the build script and .gitignore

--- beepy.zsh
+++ beepy.zsh
@@ -1,8 +1,10 @@
 #! /bin/zsh
 
+set -e
+
 # Build locally, which will build both native and ARM versions of the code
 # Also, forward args from this script to zig, so we can do things like `./beepy.zsh -Doptimize=ReleaseSafe`
 zig build $@
 
 # Copy the binaries over to the Beepy, changing the binary names
-rsync -avP zig-out/bin/*Arm beepy:~/
+rsync -avP zig-out/bin/arm-* beepy:~/
